0bdf8046844493d8ecbc4e95a9b840453b529dcd
Kevin Kenger <kevinkenger@gmail.com>
2025-02-02 13:51:35 -0700

    fix hover animations

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 // update these variables to reference shadow root instead of document when needed
 const traackrGlobalNavigation = document.getElementById('traackr-global-navigation'),
-      menuPanel = document.getElementById('flyoutWrapper');
+      menuPanel = document.getElementById('flyoutWrapper'),
+      siteHeaderArrow = document.getElementById('siteHeaderArrow');
 
 traackrGlobalNavigation.querySelectorAll('.tab').forEach(tab => {
     tab.addEventListener('mouseover', () => {
+        const distanceFromLeft = tab.getBoundingClientRect().left,
+              tabWidth = tab.getBoundingClientRect().width;
+        siteHeaderArrow.style.setProperty('--siteHeaderArrowOffset', ((distanceFromLeft + tabWidth / 2) - 6) + 'px');
+        siteHeaderArrow.classList.add('show-arrow');
+        menuPanel.style.setProperty('--translateX-value', distanceFromLeft - 80 + 'px');
+        menuPanel.classList.add('state--open');
+
         traackrGlobalNavigation.querySelectorAll('.global-navigation-tab-panel').forEach(panel => {
             const targetId = tab.getAttribute('aria-controls');
             if (panel.id == targetId) {
-                menuPanel.classList.add('state--open');
                 panel.classList.add('global-navigation-open-tab');
             } else {
                 panel.classList.remove('global-navigation-open-tab');
@@ -16,8 +23,9 @@ traackrGlobalNavigation.querySelectorAll('.tab').forEach(tab => {
     });
 
     tab.addEventListener('mouseout', e => {
-        if (e.toElement != menuPanel) {
+        if (e.toElement != menuPanel && e.toElement != siteHeaderArrow) {
             menuPanel.classList.remove('state--open');
+            siteHeaderArrow.classList.remove('show-arrow');
         }
     });
 });
@@ -30,7 +38,7 @@ menuPanel.addEventListener('mouseout', e => {
         a = a.parentNode;
     }
 
-    if (!els.includes('flyoutWrapper')) {
+    if (!els.includes('flyoutWrapper') && !els.includes('siteHeaderArrow')) {
         menuPanel.classList.remove('state--open');
     }
 });
