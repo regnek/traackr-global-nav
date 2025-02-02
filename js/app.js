dd0123c584926dca1da02489c9ef51d35eee870a
Kevin Kenger <kevinkenger@gmail.com>
2025-02-01 23:17:45 -0700

    localize js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,36 @@
-const traackrGlobalNavigation = document.getElementById('traackr-global-navigation');
+const traackrGlobalNavigation = document.getElementById('traackr-global-navigation'),
+      menuPanel = document.getElementById('flyoutWrapper');
 
 traackrGlobalNavigation.querySelectorAll('.tab').forEach(tab => {
-    tab.addEventListener('mouseover', e => {
+    tab.addEventListener('mouseover', () => {
         traackrGlobalNavigation.querySelectorAll('.global-navigation-tab-panel').forEach(panel => {
             const targetId = tab.getAttribute('aria-controls');
             if (panel.id == targetId) {
+                menuPanel.classList.add('state--open');
                 panel.classList.add('global-navigation-open-tab');
             } else {
                 panel.classList.remove('global-navigation-open-tab');
             }
         });
     });
+
+    tab.addEventListener('mouseout', e => {
+        if (e.toElement != menuPanel) {
+            menuPanel.classList.remove('state--open');
+        }
+    });
+});
+
+menuPanel.addEventListener('mouseout', e => {
+    let a = e.toElement;
+    const els = [];
+    while (a) {
+        els.unshift(a.id);
+        a = a.parentNode;
+    }
+
+    if (!els.includes('flyoutWrapper')) {
+        menuPanel.classList.remove('state--open');
+    }
+
 });
