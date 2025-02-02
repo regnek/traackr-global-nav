e9c6a53ae00b3bf9c0e191c044f150acac063634
Kevin Kenger <kevinkenger@gmail.com>
2025-02-02 12:36:07 -0700

    update nav link styling

--- js/app.js
+++ js/app.js
@@ -1,43 +1,36 @@
-window.addEventListener('DOMContentLoaded', initGlobalNav);
+// update these variables to reference shadow root instead of document when needed
+const traackrGlobalNavigation = document.getElementById('traackr-global-navigation'),
+      menuPanel = document.getElementById('flyoutWrapper');
 
-function initGlobalNav(e, root) {
-    const srcEl = root || document;
-
-    console.log(srcEl);
-
-    const traackrGlobalNavigation = document.getElementById('traackr-global-navigation'),
-        menuPanel = document.getElementById('flyoutWrapper');
-
-    traackrGlobalNavigation.querySelectorAll('.tab').forEach(tab => {
-        tab.addEventListener('mouseover', () => {
-            traackrGlobalNavigation.querySelectorAll('.global-navigation-tab-panel').forEach(panel => {
-                const targetId = tab.getAttribute('aria-controls');
-                if (panel.id == targetId) {
-                    menuPanel.classList.add('state--open');
-                    panel.classList.add('global-navigation-open-tab');
-                } else {
-                    panel.classList.remove('global-navigation-open-tab');
-                }
-            });
-        });
-
-        tab.addEventListener('mouseout', e => {
-            if (e.toElement != menuPanel) {
-                menuPanel.classList.remove('state--open');
+traackrGlobalNavigation.querySelectorAll('.tab').forEach(tab => {
+    tab.addEventListener('mouseover', () => {
+        traackrGlobalNavigation.querySelectorAll('.global-navigation-tab-panel').forEach(panel => {
+            const targetId = tab.getAttribute('aria-controls');
+            if (panel.id == targetId) {
+                menuPanel.classList.add('state--open');
+                panel.classList.add('global-navigation-open-tab');
+            } else {
+                panel.classList.remove('global-navigation-open-tab');
             }
         });
     });
 
-    menuPanel.addEventListener('mouseout', e => {
-        let a = e.toElement;
-        const els = [];
-        while (a) {
-            els.unshift(a.id);
-            a = a.parentNode;
-        }
-
-        if (!els.includes('flyoutWrapper')) {
+    tab.addEventListener('mouseout', e => {
+        if (e.toElement != menuPanel) {
             menuPanel.classList.remove('state--open');
         }
     });
-}
+});
+
+menuPanel.addEventListener('mouseout', e => {
+    let a = e.toElement;
+    const els = [];
+    while (a) {
+        els.unshift(a.id);
+        a = a.parentNode;
+    }
+
+    if (!els.includes('flyoutWrapper')) {
+        menuPanel.classList.remove('state--open');
+    }
+});
